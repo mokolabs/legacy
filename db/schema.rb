432cfe62a93c6be32037f2fab77df58987d2425a
Patrick Crowley <patrick@mokolabs.com>
2009-02-05 12:19:33 -0800

    updated schema and added missing text helper

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090205192736) do
+ActiveRecord::Schema.define(:version => 20090205201528) do
+
+  create_table "architects", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "theaters", :force => true do |t|
     t.string   "name"
