d555d0269dc33735edc03b572694d9fcd01aa195
Patrick Crowley <patrick@mokolabs.com>
2009-02-05 16:27:03 -0800

    fleshed out working examples

--- app/models/architect.rb
+++ app/models/architect.rb
@@ -1,2 +1,7 @@
 class Architect < ActiveRecord::Base
+  
+  def name
+    self.first_name + " " + self.last_name
+  end
+
 end
--- app/models/legacy_theater.rb
+++ app/models/legacy_theater.rb
@@ -3,9 +3,27 @@ class LegacyTheater < LegacyBase
 
   def map
     {
-      :name => self.name_present,
-      :description => self.desc_long
+      :name => recombine_name(self.name_present, self.name_the),
+      :description => cleanup(self.desc_long)
     }
   end
 
+  protected
+  
+    def recombine_name(name, the)
+      if the == 'Yes'
+        "The " + name
+      else
+        name
+      end    
+    end
+    
+    def cleanup(text)
+      text = convert_double_dashes_to_em_dashes(text)
+    end
+    
+    def convert_double_dashes_to_em_dashes(text)
+      text = text.gsub(/\-\-/,'&mdash;')
+    end
+  
 end
